93514611bc5bace13c441b74c9ab7804acb25ecc
yaritakouhei <yarita_kouhei@b-engineer.com>
2017-02-22 22:18:45 +0900

    add post todo

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,25 @@ var rout = exp.Router();
 var knex = require('../models/todoModel');
 /* GET home page. */
 rout.get('/', function (req, res, next) {
+    selectTodo(res);
+});
+rout.post('/create', function (req, res, next) {
+    knex('todo').insert({
+        todo: req.body.title,
+        description: req.body.description
+    })
+        .then(function () {
+        selectTodo(res);
+    });
+    console.log(req.body.title);
+    console.log(req.body.description);
+});
+rout.delete('/delete/:id', function (req, res, next) {
+});
+var selectTodo = function (res) {
     knex.select("*").from("todo").then(function (rows) {
         res.render('index', { todos: rows });
         res.end();
     });
-});
-rout.post('/create', function () {
-});
-rout.delete('/delete/:id', function () {
-});
+};
 module.exports = rout;
--- routes/index.ts
+++ routes/index.ts
@@ -5,17 +5,29 @@ const knex = require('../models/todoModel');
 
 /* GET home page. */
 rout.get('/', function(req, res, next) {
+	selectTodo(res);
+});
+
+rout.post('/create', (req, res, next) => {
+    knex('todo').insert({
+    		todo       : req.body.title,
+    		description: req.body.description
+    	})
+    .then(() => {
+    	selectTodo(res);
+    });
+	console.log(req.body.title);
+	console.log(req.body.description);
+});
+
+rout.delete('/delete/:id', (req, res, next) => {
+
+});
+
+const selectTodo: any = (res: any) => {
 	knex.select("*").from("todo").then( (rows) => {
 		res.render('index', {todos: rows});
 		res.end();
   	});
-});
-
-rout.post('/create', () => {
-
-});
-
-rout.delete('/delete/:id', () => {
-
-});
+}
 module.exports = rout;
