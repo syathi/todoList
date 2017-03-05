9f310f6a77cc2c07fe8e693b7befed0901e8f8b0
yaritakouhei <yarita_kouhei@b-engineer.com>
2017-03-06 02:54:00 +0900

    add delete func

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ rout.get('/', function (req, res, next) {
     selectTodo(res);
 });
 rout.post('/create', function (req, res, next) {
-    knex('todo').insert({
+    knex("todo").insert({
         todo: req.body.title,
         description: req.body.description
     })
@@ -19,9 +19,14 @@ rout.post('/create', function (req, res, next) {
 //HACK:this is not RESTful. chenge to delete method later.
 rout.post('/delete/:id', function (req, res, next) {
     //knex('todo').where()
-    console.log(req.body);
-    res.end();
+    knex("todo").select("*").from("todo").where("id", req.params.id).del()
+        .then(function (num) {
+        if (num <= 0)
+            console.log("may be you couldn't delete data");
+        selectTodo(res);
+    });
 });
+//redirect to top page (finally change SPA ,so probubly i will delete it )
 var selectTodo = function (res) {
     knex.select("*").from("todo").then(function (rows) {
         res.render('index', { todos: rows });
--- routes/index.ts
+++ routes/index.ts
@@ -9,7 +9,7 @@ rout.get('/', function(req, res, next) {
 });
 
 rout.post('/create', (req, res, next) => {
-    knex('todo').insert({
+    knex("todo").insert({
     		todo       : req.body.title,
     		description: req.body.description
     	})
@@ -23,10 +23,14 @@ rout.post('/create', (req, res, next) => {
 //HACK:this is not RESTful. chenge to delete method later.
 rout.post('/delete/:id', (req, res, next) => {
 	//knex('todo').where()
-	console.log(req.body);
-	res.end();
+	knex("todo").select("*").from("todo").where("id", req.params.id).del()
+	.then( (num) => {
+			if(num <= 0)console.log("may be you couldn't delete data");
+			selectTodo(res);
+		});
 });
 
+//redirect to top page (finally change SPA ,so probubly i will delete it )
 const selectTodo: (res: any) => void = (res: any): void => {
 	knex.select("*").from("todo").then( (rows) => {
 		res.render('index', {todos: rows});
